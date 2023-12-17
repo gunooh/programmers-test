2870be78d9d94a84dfbe138a5f8a3a8a92e20b6e
gunooh <gunooh12@gmail.com>
2023-12-17 12:34:05 +0900

    [programmers] LV.0 - 369게임

--- LV0/120891.js
+++ LV0/120891.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /**
  * 369게임
  * https://school.programmers.co.kr/learn/courses/30/lessons/120891
@@ -7,10 +8,16 @@
  * 머쓱이가 말해야하는 숫자 order가 매개변수로 주어질 때, 머쓱이가 쳐야할 박수 횟수를 return 하도록 solution 함수를 완성해보세요.
  */
 
-const solution = (order) => {
-  // console.log([...String(order)]);
+// 내 풀이
+const solution = (order) => [...String(order)].reduce((acc, cur) => {
+  if (Number(cur) !== 0 && Number(cur) % 3 === 0) {
+    acc += 1;
+  }
+  return acc;
+}, 0);
 
-};
+// 다른 풀이
+// const solution = (order) => [...String(order).matchAll(/[369]/g)].length;
 
-const order = 29423;
+const order = 300;
 console.log(solution(order));
