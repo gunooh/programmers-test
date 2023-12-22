205885761952d771a2d29aed8fd673ebad406f58
gunooh <gunooh12@gmail.com>
2023-12-22 19:29:58 +0900

    [programmers] LV.1 - 문자열을 정수로 바꾸기

---
+++ LV1/12925.js
@@ -0,0 +1,11 @@
+/**
+ * 문자열을 정수로 바꾸기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12925
+ *
+ * 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+ */
+
+const solution = (str) => Number(str);
+
+const str = '1234';
+console.log(solution(str));
