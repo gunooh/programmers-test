3cfa24d34dc1905e2f7aee5fdcf7453c5c60c0d6
gunooh <gunooh12@gmail.com>
2025-05-10 14:10:22 +0900

    [programmers] LV.0 - 그림 확대

---
+++ LV0/181836.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-plusplus */
+/**
+ * 그림 확대
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181836
+ *
+ * 직사각형 형태의 그림 파일이 있고, 이 그림 파일은 1 × 1 크기의 정사각형 크기의 픽셀로 이루어져 있습니다.
+ * 이 그림 파일을 나타낸 문자열 배열 picture과 정수 k가 매개변수로 주어질 때,
+ * 이 그림 파일을 가로 세로로 k배 늘린 그림 파일을 나타내도록 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+ *
+ */
+
+// const solution = (picture, k) => {
+//   const result = picture.reduce((acc, item) => {
+//     for (let i = 0; i < k; i++) {
+//       acc.push(item.split('').map((char) => char.repeat(k)).join(''));
+//     }
+//     return acc;
+//   }, []);
+
+//   return result;
+// };
+
+// 다른 풀이
+const solution = (picture, k) => picture.flatMap((row) => {
+  const expandedRow = [...row].reduce((acc, cur) => acc + cur.repeat(k), '');
+
+  return new Array(k).fill(expandedRow);
+});
+
+const picture = ['.xx...xx.', 'x..x.x..x', 'x...x...x', '.x.....x.', '..x...x..', '...x.x...', '....x....'];
+const k = 2;
+console.log(solution(picture, k));
