2b974aac939a1f2ced198a621c9f729968373b54
gunooh <gunooh12@gmail.com>
2025-05-11 20:07:30 +0900

    [programmers] LV.2 - 타겟 넘버

---
+++ LV2/43165.js
@@ -0,0 +1,58 @@
+/**
+ * 타겟 넘버
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43165
+ *
+ * 문제 설명
+ * n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+ * -1+1+1+1+1 = 3
+ * +1-1+1+1+1 = 3
+ * +1+1-1+1+1 = 3
+ * +1+1+1-1+1 = 3
+ * +1+1+1+1-1 = 3
+ * 사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 완성해주세요.
+ */
+
+// dfs
+const solution = (numbers, target) => {
+  let answer = 0;
+  const dfs = (index, sum) => {
+    if (index === numbers.length) {
+      if (sum === target) answer += 1;
+      return;
+    }
+
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  };
+
+  dfs(0, 0);
+  return answer;
+};
+
+// bfs
+// const solution = (numbers, target) => {
+//   let answer = 0;
+//   const queue = [{ index: 0, sum: 0 }];
+
+//   const bfs = () => {
+//     while (queue.length > 0) {
+//       const { index, sum } = queue.shift();
+
+//       if (index === numbers.length) {
+//         if (sum === target) answer += 1;
+//         continue;
+//       }
+
+//       queue.push({ index: index + 1, sum: sum + numbers[index] });
+//       queue.push({ index: index + 1, sum: sum - numbers[index] });
+//     }
+//   };
+
+//   bfs();
+
+//   return answer;
+// };
+
+const numbers = [1, 1, 1, 1, 1];
+const target = 3;
+console.log(solution(numbers, target));
